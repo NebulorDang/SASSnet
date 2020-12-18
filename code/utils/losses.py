313e0e6b1f9da625b17719a6bc4c0cf5db2a8f91
polpol <369671231@qq.com>
2020-12-18 16:24:30 +0800

    modify comment

--- code/utils/losses.py
+++ code/utils/losses.py
@@ -132,10 +132,16 @@ class FocalLoss(nn.Module):
 
         logpt = F.log_softmax(input, dim=1)
 
-        # 这里因为第1维为Class维度,而target中的元素只可能为0或者1,所以output[i][j] = input[i][target[i][j]]
-        # target[i][j]正好是类别的值0或者1,所以采用gather函数
+        # 这里因为第1维为Class维度,而target中的元素为类别的索引,所以output[i][j] = input[i][target[i][j]]
+        # target[i][j]正好是类别的值,所以采用gather函数
 
+        # gather函数中索引维度与输出维度是一样的,输入维度不影响输出维度
+        # 按照target把target中的值当作是one-hot中的hot类别，将他们gather到一起
+        # 这样得到 n * 1 的向量聚合了每个像素点上target标识的实际所属类的预测为该类的概率值
+        
         logpt = logpt.gather(1,target)
+
+        #转为 n * 1 维度保持和之后算出的at维度一致,才能够点乘
         logpt = logpt.view(-1)
 
         # 这里用data先剥离出来再用Variable嵌入到计算图中，
@@ -150,6 +156,8 @@ class FocalLoss(nn.Module):
             # output[i][j] = input[target[i][j]][j]
             # target[i][j]正好是类别的值0或者1,0代表正样本则使用alpha,1代表负样本则使用1-alpha
             # 所以gather的维度为0
+            # 得到 1 * n 维的向量聚合了每个像素点上target标识的实际所属类的权重alpha,如果第一类为alpha
+            # 那么其他类别都为  1 - alpha
             at = self.alpha.gather(0,target.data.view(-1))
 
             # 为什么alpha需要梯度呢?Variable不能与非Variable相乘么
@@ -158,7 +166,6 @@ class FocalLoss(nn.Module):
         loss = -1 * (1-pt)**self.gamma * logpt
         if self.size_average: return loss.mean()
         else: return loss.sum()
-
 
 class FocalLoss2(nn.Module):
     r"""
@@ -180,7 +187,7 @@ class FocalLoss2(nn.Module):
 
     """
     def __init__(self, class_num, alpha=None, gamma=2, size_average=True):
-        super(FocalLoss, self).__init__()
+        super(FocalLoss2, self).__init__()
         if alpha is None:
             self.alpha = Variable(torch.ones(class_num, 1))
         else:
@@ -200,16 +207,26 @@ class FocalLoss2(nn.Module):
         class_mask = inputs.data.new(N, C).fill_(0)
         class_mask = Variable(class_mask)
         ids = targets.view(-1, 1)
+
+        #这里是将每一类的标签分出来，散列到class维度上的不同矩阵中
         class_mask.scatter_(1, ids.data, 1.)
         #print(class_mask)
 
-
         if inputs.is_cuda and not self.alpha.is_cuda:
             self.alpha = self.alpha.cuda()
+        
+        # 将alpha按照target中的类别索引重排,得到每个像素点上标识的类别对应的alpha系数
+        # 可以置换为FocalLoss第一种实现中使用gather的方式
         alpha = self.alpha[ids.data.view(-1)]
 
+        # class是按照类别散列开的每一类的掩码,P是softmax概率,这种操作将softmax概率乘到
+        # 每一类上的掩码上去,再按照类别维度加和起来(相当于压扁到一张map上面)得到每个像素点target标识的
+        # class的预测概率
+        # 这里要转为和alpha维度一致,alpha[ids.data.view(-1)]是行向量
+        # probs = (P*class_mask).sum(1).view(-1,1) 是列向量，这里可能有误
+        # 行向量与列向量点乘会导致列向量复制多份，然后变成行向量乘矩阵
         probs = (P*class_mask).sum(1).view(-1,1)
-
+    
         log_p = probs.log()
         #print('probs size= {}'.format(probs.size()))
         #print(probs)
@@ -218,9 +235,58 @@ class FocalLoss2(nn.Module):
         #print('-----bacth_loss------')
         #print(batch_loss)
 
-
         if self.size_average:
             loss = batch_loss.mean()
         else:
             loss = batch_loss.sum()
+        return loss
+
+class FocalLoss3(nn.Module):    
+    def __init__(self, alpha=0.25, gamma=2, num_classes = 3, size_average=True):
+        """
+        参考博客 https://ptorch.com/news/253.html
+        focal_loss损失函数, -α(1-yi)**γ *ce_loss(xi,yi)      
+        步骤详细的实现了 focal_loss损失函数.
+        :param alpha:   阿尔法α,类别权重.      当α是列表时,为各类别权重,当α为常数时,类别权重为[α, 1-α, 1-α, ....],常用于 目标检测算法中抑制背景类 , retainnet中设置为0.25
+        :param gamma:   伽马γ,难易样本调节参数. retainnet中设置为2
+        :param num_classes:     类别数量
+        :param size_average:    损失计算方式,默认取均值
+        """
+
+        super(FocalLoss3,self).__init__()
+        self.size_average = size_average
+        if isinstance(alpha,list):
+            assert len(alpha)==num_classes   # α可以以list方式输入,size:[num_classes] 用于对不同类别精细地赋予权重
+            print("Focal_loss alpha = {}, 将对每一类权重进行精细化赋值".format(alpha))
+            self.alpha = torch.Tensor(alpha)
+        else:
+            assert alpha<1   #如果α为一个常数,则降低第一类的影响,在目标检测中为第一类
+            print(" --- Focal_loss alpha = {} ,将对背景类进行衰减,请在目标检测任务中使用 --- ".format(alpha))
+            self.alpha = torch.zeros(num_classes)
+            self.alpha[0] += alpha
+            self.alpha[1:] += (1-alpha) # α 最终为 [ α, 1-α, 1-α, 1-α, 1-α, ...] size:[num_classes]
+        self.gamma = gamma
+
+    def forward(self, preds, labels):
+        """
+        focal_loss损失计算        
+        :param preds:   预测类别. size:[B,N,C] or [B,C]    分别对应与检测与分类任务, B 批次, N检测框数, C类别数        
+        :param labels:  实际类别. size:[B,N] or [B]        
+        :return:
+        """        
+        # assert preds.dim()==2 and labels.dim()==1        
+        preds = preds.view(-1,preds.size(-1))        
+        self.alpha = self.alpha.to(preds.device)        
+        preds_softmax = F.softmax(preds, dim=1) # 这里并没有直接使用log_softmax, 因为后面会用到softmax的结果(当然你也可以使用log_softmax,然后进行exp操作)        
+        preds_logsoft = torch.log(preds_softmax)
+        preds_softmax = preds_softmax.gather(1,labels.view(-1,1))   # 这部分实现nll_loss ( crossempty = log_softmax + nll )        
+        preds_logsoft = preds_logsoft.gather(1,labels.view(-1,1))        
+        self.alpha = self.alpha.gather(0,labels.view(-1))        
+        loss = -torch.mul(torch.pow((1-preds_softmax), self.gamma), preds_logsoft)  # torch.pow((1-preds_softmax), self.gamma) 为focal loss中 (1-pt)**γ
+        #原本loss是 n * 1 维度的向量,alpha是 1 * n 维度的向量,这里loss.t()先转置后点乘
+        loss = torch.mul(self.alpha, loss.t())        
+        if self.size_average:        
+            loss = loss.mean()        
+        else:            
+            loss = loss.sum()        
         return loss
